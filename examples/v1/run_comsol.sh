9dc7f802f0021b98bafd29c32f2c309efdf4b0e3
nilsvanrompaey <nils.vanrompaey@student.kuleuven.be>
2023-02-27 14:26:44 +0100

    SImulations

--- examples/v1/run_comsol.sh
+++ examples/v1/run_comsol.sh
@@ -2,8 +2,7 @@
 source /users/students/r0754386/Documents/bmtk/bin/activate
 module load mpi
 cd /users/students/r0754386/Documents/bmtk/examples/v1
-mpirun -np 12 nrniv -mpi -python run_bionet.py config_comsol_0_0.json --no_lgn False
-mpirun -np 12 nrniv -mpi -python run_bionet.py config_comsol_0_0_LGN.json 
-mpirun -np 12 nrniv -mpi -python run_bionet.py config_comsol_400_400.json --no_lgn False
-mpirun -np 12 nrniv -mpi -python run_bionet.py config_comsol_-400_-400.json --no_lgn False
+mpirun -np 12 nrniv -mpi -python run_bionet.py config_comsol_0_0.json --overwrite True
+mpirun -np 12 nrniv -mpi -python run_bionet.py config_comsol_400_400.json
+mpirun -np 12 nrniv -mpi -python run_bionet.py config_comsol_-400_-400.json
 
