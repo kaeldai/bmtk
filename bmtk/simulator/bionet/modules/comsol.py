b7c362dda61ff0f7f062296f4c23ba51f52d180a
nilsvanrompaey <nils.vanrompaey@student.kuleuven.be>
2023-03-05 16:44:52 +0100

    Option to specify waveform outside COMSOL

--- bmtk/simulator/bionet/modules/comsol.py
+++ bmtk/simulator/bionet/modules/comsol.py
@@ -12,24 +12,25 @@ from bmtk.simulator.bionet.modules.xstim_waveforms import stimx_waveform_factory
 class ComsolMod(SimulatorMod):
     """ 
     __init__: COMSOL output .txt file is loaded as pandas dataframe and then used to set up nearest neighbour (NN) interpolation object to create interpolation map
-    :param comsol_file: path of .txt file
-    :param waveform: if specified, waveform of stimulation can be specified
+    :param comsol_file: path of .txt file. Coordinates in [um], potentials in [mV], timepoints in [s].
+    :param waveform: path of .csv file as created with examples/bio_components/waveform.py
+                    If specified, comsol_file should contain output from stationary study and waveform is defined through this parameter.
+                    If not specified, comsol_file should contain output from time-dependent study.
 
     initialise: An interpolation map is defined of every segment and stored in dictionary self._NN, done iteratively for every cell/gid.
-    The interpolation map maps (the center of) every segment to its NN. It is calculated once here and then used in every step. 
+    The interpolation map points (the center of) every segment to its NN. It is calculated once here and then used in every step. 
     Next, the COMSOL output is also interpolated in time to match the timesteps in BMTK.
 
     step: The interpolation map is used to point each segment to its NN and find the corresponding voltage value in the comsol df.
-
     """
+
     def __init__(self, comsol_file, waveform=None, cells=None, set_nrn_mechanisms=True,
                  node_set=None):
 
-        # TO DO: add possibility to specify time dependency in bmtk rather than COMSOL?
-        # if waveform is not None:
-        #     self._waveform = stimx_waveform_factory(waveform)
-        # else:
-        #     self._waveform = None
+        if waveform is not None:
+            self._waveform = stimx_waveform_factory(waveform)
+        else:
+            self._waveform = None
         
         self._comsol_file = comsol_file
 
@@ -38,10 +39,13 @@ class ComsolMod(SimulatorMod):
         header[0] = header[0][2:]                               # remove '% ' before first column name
         for i,col in enumerate(header):                         # remove superfluous characters before actual time value
             if col[0] == "V":
-                header[i] = float(col[11:])
+                if self._waveform is None:
+                    header[i] = float(col[11:])
+                else:
+                    header[i] = 0
         self._timepoints = np.array(header[3:], dtype=float)    # create array of timepoints  
 
-        # load actual values in COMSOL output .txt file.  
+        # load data in COMSOL output .txt file.  
         self._comsol = pd.read_csv(comsol_file, sep="\s+", header=None, skiprows=9, names=header)       # load data from .txt file
         self._NNip = NNip(self._comsol[['x','y','z']], np.arange(len(self._comsol['x'])))               # create scipy NN interpolation object 
         self._NN = {}                                           # initialise empty dictionary that will contain NN map of each cell
@@ -65,25 +69,31 @@ class ComsolMod(SimulatorMod):
 
             # spatial interpolation
             r05 = cell.seg_coords.p05               # get position of middle of segment
-            self._NN[gid] = self._NNip(r05.T)       # find and save nearest node in COMSOL output 
+            self._NN[gid] = self._NNip(r05.T)       # get nearest COMSOL node
 
-        # temporal interpolation
-        dt = sim.dt
-        tsteps = np.arange(self._timepoints[0], self._timepoints[-1]+dt, dt/1000)   # interpolate time axis
-        self._arr = np.zeros((self._comsol.shape[0],len(tsteps)))                   # initialise empty array
-        for i in range(self._comsol.shape[0]):                                      # update each row (corresponds to a node) of the array with the time-interpolated values
-            self._arr[i,:] = np.interp(tsteps, self._timepoints, self._comsol.iloc[i,3:]).flatten()
+        if self._waveform is None:
+            # temporal interpolation
+            dt = sim.dt/1000                                                            # BMTK uses [ms], COMSOL uses [s]
+            tsteps = np.arange(self._timepoints[0], self._timepoints[-1]+dt, dt)        # interpolate time axis
+            self._arr = np.zeros((self._comsol.shape[0],len(tsteps)))                   # initialise empty array
+            for i in range(self._comsol.shape[0]):                                      # update each row (corresponding to a COMSOL node) of the array with the time-interpolated values
+                self._arr[i,:] = np.interp(tsteps, self._timepoints, self._comsol.iloc[i,3:]).flatten()
         
+        else:
+            self._arr = self._comsol[0].to_numpy().flatten()
 
     def step(self, sim, tstep):
         for gid in self._local_gids:
             cell = sim.net.get_cell_gid(gid)        # get cell gid
             NN = self._NN[gid]                      # vector that points each node of the cell to its nearest neighbour in the .txt file
-            tstep = tstep % 200
-            v_ext = self._arr[NN,tstep+1]           # assign extracellular potential value of NN at tstep
 
-            # TO DO: add possibility to specify time dependency in bmtk rather than COMSOL?
-            # if self._waveform is not None:
-            #     v_ext *= self._waveform.calculate(tstep+1)
+            if self._waveform is None:
+                T = int(1000*self._timepoints[-1]/sim.dt)
+                tstep = tstep % T                   # In case of periodic stimulation
+                v_ext = self._arr[NN,tstep+1]       # assign extracellular potential value of NN at tstep
+            else:
+                T = int(self._waveform.definition["time"].iloc[-1])
+                tstep = tstep % T                   # In case of periodic stimulation
+                v_ext = self._arr[NN]*self._waveform.calculate(tstep+1)
 
             cell.set_e_extracellular(h.Vector(v_ext))
