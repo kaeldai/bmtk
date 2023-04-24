cbd48450e3015ec50b85ad63fca76f5c395bca0f
nilsvanrompaey <nils.vanrompaey@student.kuleuven.be>
2023-04-24 16:44:59 +0200

    figures exp1

--- examples/v1/exp1/plot_output.py
+++ examples/v1/exp1/plot_output.py
@@ -46,56 +46,63 @@ config_file = 'config.json'
 #     v1=True,
 #     show=False,
 # )
+t=200
+for i in range(1,8):
+    if i == 3:
+        continue
+    i = str(i)
+    for j in range (int(i)+1,9): 
+        if j == 3:
+            continue
+        j = str(j)
+        grid1 = plot_activity_2d_smooth(**plotting_params(i,'-','30',['0','1','2']),
+            v1=True,
+            show=False,
+            t_stop=t,
+            save_dir=True
+        )
 
-for t in [200]: 
-    grid1 = plot_activity_2d_smooth(**plotting_params('2','-','30',['0','1','2']),
-        v1=True,
-        show=False,
-        t_stop=t,
-        save_dir=True
-    )
+        grid2 = plot_activity_2d_smooth(**plotting_params(j,'-','30',['0','1','2']),
+            v1=True,
+            show=False,
+            t_stop=t,
+            save_dir=True
+        )
 
-    grid2 = plot_activity_2d_smooth(**plotting_params('6','-','30',['0','1','2']),
-        v1=True,
-        show=False,
-        t_stop=t,
-        save_dir=True
-    )
+        fig = plt.figure(figsize=(9,12))
+        ax1 = plt.subplot(1,2,1)
+        grid = grid1-grid2
+        
+        # grid = np.abs(grid)
 
-    fig = plt.figure(figsize=(9,12))
-    ax1 = plt.subplot(1,2,1)
-    grid = grid1-grid2
-  
-    # grid = np.abs(grid)
-
-    # for i in range(len(grid)):
-    #     for j in range(len(grid)):
-    #         if grid[i,j,1] < 0:
-    #             grid[i,j,:] = -grid[i,j,1]
-    #         else:
-    #             grid[i,j,:] = grid[i,j,1]
+        # for i in range(len(grid)):
+        #     for j in range(len(grid)):
+        #         if grid[i,j,1] < 0:
+        #             grid[i,j,:] = -grid[i,j,1]
+        #         else:
+        #             grid[i,j,:] = grid[i,j,1]
 
 
-    ax1.imshow(grid)
+        ax1.imshow(grid)
 
-    ax2 = plt.subplot(1,2,2)
-    grid = grid2-grid1
-    ax2.imshow(grid)
-
-    # plot_activity_2d(
-    #     nodes_dir = 'networks_rebuilt/network25/v1_nodes.h5',
-    #     electrodes_dir = '../bio_components/stimulations/elec_loc/1.csv',
-    #     spikes_dir = 'exp1/output/1/-/30/01-_30_0/spikes.csv',
-    #     spikes_bg_dir = 'exp1/output/bkg/bkg_0/spikes.csv',
-    #     v1=True,
-    #     show=False,
-    # )
+        ax2 = plt.subplot(1,2,2)
+        grid = grid2-grid1
+        ax2.imshow(grid)
+        plt.savefig(i+j)
+        # plot_activity_2d(
+        #     nodes_dir = 'networks_rebuilt/network25/v1_nodes.h5',
+        #     electrodes_dir = '../bio_components/stimulations/elec_loc/1.csv',
+        #     spikes_dir = 'exp1/output/1/-/30/01-_30_0/spikes.csv',
+        #     spikes_bg_dir = 'exp1/output/bkg/bkg_0/spikes.csv',
+        #     v1=True,
+        #     show=False,
+        # )
 
     print(centroid_cov(
         nodes_dir = 'networks_rebuilt/network25/v1_nodes.h5',
         spikes_dir = 'exp1/output/1/-/30/01-_30_0/spikes.csv',
         spikes_bg_dir = 'exp1/output/bkg/bkg_0/spikes.csv',
-        t_stop = t,
+        t_stop = 200,
         v1 = True
     )[0])
 
@@ -111,7 +118,7 @@ for t in [200]:
 
 
 
-plt.show(block=False)
+# plt.show(block=False)
 plt.pause(0.001)
 input('')
 plt.close('all')
